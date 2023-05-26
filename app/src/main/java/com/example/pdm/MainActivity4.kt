b2d19194a7dae505bca4a4c7026134b387d92969
Rafael Matesco <matescorapha@gmail.com>
2023-05-25 21:23:28 -0300

    update

--- app/src/main/java/com/example/pdm/MainActivity4.kt
+++ app/src/main/java/com/example/pdm/MainActivity4.kt
@@ -1,7 +1,9 @@
 package com.example.pdm
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -18,7 +20,11 @@ class MainActivity4 : AppCompatActivity() {
     }
 
     fun init(){
-
+        var btnVoltar: Button = findViewById(R.id.btnVoltar)
+        btnVoltar.setOnClickListener{
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
     }
 
     fun mudarSenha(view: View){
